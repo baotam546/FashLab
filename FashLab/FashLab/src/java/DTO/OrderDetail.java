9054aeae166e02116efb5bd306dcc81d8f399df4
Luongntd <130530489+camkionlineks123@users.noreply.github.com>
2023-04-25 15:48:42 +0700

    update orderdetailDAO

---
+++ FashLab/FashLab/src/java/DTO/OrderDetail.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author luong
+ */
+public class OrderDetail {
+    int id;
+    int userId;
+    float total ;
+    Date createdAt;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(int id, int userId, float total, Date createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.total = total;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
+}
