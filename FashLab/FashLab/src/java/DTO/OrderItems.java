4f50768e73c3facde2860ec509ea24fec6efb076
mainht <mainhtse171497@fpt.edu.vn>
2023-04-25 10:03:03 +0700

    push OrderItems

--- FashLab/FashLab/src/java/DTO/OrderItems.java
+++ FashLab/FashLab/src/java/DTO/OrderItems.java
@@ -5,10 +5,63 @@
  */
 package DTO;
 
+import java.sql.Date;
+
 /**
  *
  * @author ACER
  */
 public class OrderItems {
-    
+
+    int id;
+    int orderId;
+    int productId;
+    Date createAt;
+
+    public OrderItems() {
+    }
+
+    public OrderItems(int id, int orderId, int productId, Date createAt) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.createAt = createAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItems{" + "id=" + id + ", orderId=" + orderId + ", productId=" + productId + ", createAt=" + createAt + '}';
+    }
 }
